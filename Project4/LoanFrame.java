5a9022e0b5e23c3ae53b2e74a0ba8645f4a487cc
Victor <mynameisvictor@126.com>
2016-12-02 14:55:05 -0500

    LoanFrame

---
+++ Project4/LoanFrame.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class LoanFrame {
+	public LoanFrame(){
+		
+	}
+	
+	private class ButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+}
